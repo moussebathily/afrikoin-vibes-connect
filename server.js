790e862840c8c2181f5a0e197519cf84ec58d5db
Mousse bathily <bathilymousse24@gmail.com>
2025-08-16 00:46:07 +0200

    Create server.js
    import express from 'express';
    import path from 'path';
    import { fileURLToPath } from 'url';
    const __dirname = path.dirname(fileURLToPath(import.meta.url));
    const app = express();
    app.use(express.static(path.join(__dirname, 'dist')));
    app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
    const port = process.env.PORT || 8080;
    app.listen(port, () => console.log(`listening on ${port}`));
    
    
    Signed-off-by: Mousse bathily <bathilymousse24@gmail.com>

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`listening on ${port}`));
